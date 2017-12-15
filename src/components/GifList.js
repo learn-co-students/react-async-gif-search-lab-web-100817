d90ed9ae9ee323cf5f815d35642f1cea01840003
Dakota Lillie <dakota.lillie@icloud.com>
2017-12-15 14:15:51 -0500

    Done.

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -1,17 +1,34 @@
 import React from "react";
+import CSSTransitionGroup from "react-addons-css-transition-group";
 
 const GifList = ({ gifs }) => {
   // embed URL, id
   const myGifs = gifs
     ? gifs.map(gif => (
         <li key={gif.id}>
-          <img src={gif.images.fixed_height.url} alt={gif.images.title} />
+          <CSSTransitionGroup
+            transitionName="example"
+            transitionAppear={true}
+            transitionAppearTimeout={500}
+            transitionEnter={true}
+            transitionEnterTimeout={500}
+            transitionLeave={true}
+            transitionLeaveTimeout={500}
+          >
+            <img src={gif.images.fixed_height.url} alt={gif.title} />
+          </CSSTransitionGroup>
         </li>
       ))
     : null;
-  return <div>
-    {gifs ? <ul className="list-unstyled">{myGifs}</ul> : <h2>Search to get started!</h2>}
-  </div>
+  return (
+    <div>
+      {gifs ? (
+        <ul className="list-unstyled">{myGifs}</ul>
+      ) : (
+        <h2>Search to get started!</h2>
+      )}
+    </div>
+  );
 };
 
 export default GifList;
